78212ace4eb04a881101f12382d664201389b3ce
mouadisfree <mokatfi13@gmail.com>
2025-12-03 16:54:03 +0100

    c++01

--- learn/program.cpp
+++ learn/program.cpp
@@ -1,9 +1,88 @@
-#include "header.hpp"
-#include "header.hpp"
+#include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello world" << std::endl;
-    return 0;
+class   Harl
+{
+private:
+    void    debug( void );
+    void    info( void );
+    void    warning( void );
+    void    error( void );
+public:
+    Harl();
+    ~Harl();
+
+    void        complain( std:: string level );
+};
+
+
+
+
+void    Harl::debug( void ) {
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+    std::cout << std::endl;
 }
 
-#include "header.hpp"
+void    Harl::info( void ) {
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+    std::cout << std::endl;
+}
+
+void    Harl::warning( void ) {
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming foryears whereas you started working here since last month.\n ";
+    std::cout << std::endl;
+}
+
+void    Harl::error( void ) {
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+    std::cout << std::endl;
+}
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void    Harl::complain( std:: string level ) 
+{
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+    int i = 0;
+    while (i < 4 && levels[i].compare(level))
+        i++ ;
+    switch (i)
+    {
+    case    0:
+        this->debug();
+    case    1: 
+        this->info();
+    case    2: 
+        this->warning();
+    case    3: 
+        this->error();
+        break ;
+    default:
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+}
+
+
+int main( int ac, char **av ) 
+{
+
+    std::string input = av[1];
+    Harl        harl;
+    
+    if (ac != 2) 
+    {
+        return (std::cout << "Usage: ./harlFilter \"level\"\n", EXIT_FAILURE);
+    }
+    harl.complain(input);
+    return (0);
+}
